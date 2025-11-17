10a5be7d4c0e243a58dadc4fca26737a9e43099c
Shad0wKilleer <bsai23001@itu.edu.pk>
2025-11-18 00:45:47 +0500

    Updated the path for training data

--- Dataset_save_csv.py
+++ Dataset_save_csv.py
@@ -111,7 +111,7 @@ def create_data_csv(root):
 
 
 if __name__ == "__main__":
-    trainset_dir = "/content/drive/My Drive/Renal Segmentation/HATs_data/val"
+    trainset_dir = "/content/drive/My Drive/Renal Segmentation/HATs_data/train"
 
     print(f"Starting script. Scanning directory: {trainset_dir}")
     create_data_csv(root=trainset_dir)
